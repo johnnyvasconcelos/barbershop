7635b6373c3e28c812055bb0e55408e120c5b789
Jhonatan <jvasconcelosbittencourt@gmail.com>
2026-06-27 16:52:15 -0300

    ajustes

--- barbershop/src/app/layout.tsx
+++ barbershop/src/app/layout.tsx
@@ -1,16 +1,14 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+// @ts-expect-error - TS check disabled for css import
 import "./globals.css";
-import { Geist } from "next/font/google";
-import { cn } from "@/app/_lib/utils";
-
-const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -28,9 +26,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("font-sans", geist.variable)}>
+    <html lang="pt-br">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}
       >
         {children}
       </body>
