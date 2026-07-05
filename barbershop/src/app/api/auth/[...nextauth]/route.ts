bbfda3e8eb79358e2147d2a4c604b2a0f5feaf93
Jhonatan <jvasconcelosbittencourt@gmail.com>
2026-07-05 13:01:57 -0300

    ajustes

--- barbershop/src/app/api/auth/[...nextauth]/route.ts
+++ barbershop/src/app/api/auth/[...nextauth]/route.ts
@@ -1,28 +1,5 @@
-import { db } from "@/app/_lib/prisma";
-import NextAuth, { AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { Adapter } from "next-auth/adapters";
-
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(db) as Adapter,
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  callbacks: {
-    async session({ session, user }) {
-      session.user = {
-        ...session.user,
-        id: user.id,
-      } as any;
-
-      return session;
-    },
-  },
-};
+import NextAuth from "next-auth";
+import { authOptions } from "@/app/_lib/auth";
 
 const handler = NextAuth(authOptions);
 
